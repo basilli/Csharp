a350bcb18e52950f40b368d61f42ed8025b1f5e2
DESKTOP-5V3FRMH\M <basill7813@gmail.com>
2025-02-26 17:16:57 +0900

    1ㅂ2ㅈ3ㄷ4ㄱ

---
+++ Assets/Scripts/HelloWorld/HelloWorldPractice.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class HelloWorldPractice : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello World!!!");
+        Debug.Log("123456789");
+        Debug.Log("1¤²2¤¸3¤§4¤¡");
+        Debug.Log("«¸«ã«¬«¤«â ªÈ «µ«Ä«Ş«¤«â");
+        Debug.Log("êúóøûúØ«÷èÔçİ¯");
+    }
+
+}
+
+/*
+ [output] : ÄÜ¼ÖÃ¢¿¡ ¾Æ·¡ ³»¿ëÀ» Ãâ·ÂÇÏ¼¼¿ä
+Hello Unity!!!
+123456789
+1¤²2¤¸3¤§
+*/
